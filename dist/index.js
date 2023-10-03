647d377bfe7fa8c6857ad3bddeeb8ad81dbef0fd
Dan <bogdanlinee@gmail.com>
2023-10-03 09:15:46 +0300

    3.3

--- dist/index.js
+++ dist/index.js
@@ -2,3 +2,51 @@
 // Task 1 ============================================================
 //====================================================================
 const a = "Add";
+// Task 3 ============================================================
+//====================================================================
+function summ(a) {
+    if (!a) {
+        return 2022;
+    }
+    const x = Object.keys(a).map((k) => {
+        const elem = a[k];
+        if (typeof elem === undefined) {
+            return 2022;
+        }
+        if (typeof elem.cvalue === 'object') {
+            return summ(elem.cvalue);
+        }
+        if (typeof elem.cvalue === 'string') {
+            return +elem.cvalue || 2022;
+        }
+        if (typeof elem.cvalue === 'undefined') {
+            return 2022;
+        }
+        return elem.cvalue;
+    });
+    if (x.indexOf(2022) !== -1) {
+        return 2022;
+    }
+    let sum = 0;
+    for (let i = 0; i < x.length; i++) {
+        sum += x[i];
+    }
+    return sum;
+}
+let result = summ({
+    hello: {
+        cvalue: 2
+    },
+    world: {
+        cvalue: {
+            yay: {
+                cvalue: {
+                    yay: {
+                        cvalue: "2"
+                    }
+                }
+            }
+        }
+    }
+});
+console.log({ result });
--- src/index.ts
+++ src/index.ts
@@ -19,3 +19,87 @@ const a: AnyButtonType = "Add";
 type ConfirmationHandlingFormButton = {
     onConfirm: (param: DialogButtonType) => void
 }
+
+// Task 3 ============================================================
+//====================================================================
+
+function summ(a: SumType2) {
+    if (!a) {
+        return 2022;
+    }
+
+    const x = Object.keys(a).map((k) => {
+        const elem = a[k];
+        if (typeof elem === undefined) {
+            return 2022;
+        }
+
+        if (typeof elem.cvalue === 'object') {
+            return summ(elem.cvalue);
+        }
+
+        if (typeof elem.cvalue === 'string') {
+            return +elem.cvalue || 2022;
+        }
+
+        if (typeof elem.cvalue === 'undefined') {
+            return 2022;
+        }
+
+        return elem.cvalue;
+    });
+
+    if (x.indexOf(2022) !== -1) {
+        return 2022;
+    }
+
+    let sum = 0;
+
+    for (let i = 0; i < x.length; i++) {
+        sum += x[i];
+    }
+
+    return sum
+}
+
+type SumType1 = {
+    cvalue?: string | number | undefined | SumType1,
+    [key: string]: SumType1,
+};
+
+type SumType2 = SumType1 | undefined;
+
+let result = summ({
+    hello: {
+        cvalue: 2
+    },
+    world: {
+        cvalue: {
+            yay: {
+                cvalue: {
+                    yay: {
+                        cvalue: "2"
+                    }
+                }
+            }
+        }
+    }
+});
+
+console.log({result});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
